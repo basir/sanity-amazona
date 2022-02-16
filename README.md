27844a35122db51ab165e2a7158cf4db5113ec7f
Basir <basir.jafarzadeh@gmail.com>
2022-02-16 19:14:17 +0330

    Video-06-Connect-To-Sanity.io

--- README.md
+++ README.md
@@ -21,3 +21,8 @@
    1. Add header
    2. Add main
    3. Add footer
+6. Connect to Sanity.io
+   1. install sanity
+   2. initialize sanity
+   3. create product model
+   4. insert sample data to product model
---
+++ sanity/schemas/product.js
@@ -0,0 +1,65 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'brand',
+      title: 'Brand',
+      type: 'string',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'rating',
+      title: 'Rating',
+      type: 'number',
+    },
+
+    {
+      name: 'numReviews',
+      title: 'NumReviews',
+      type: 'number',
+    },
+    {
+      name: 'countInStock',
+      title: 'CountInStock',
+      type: 'number',
+    },
+  ],
+};
